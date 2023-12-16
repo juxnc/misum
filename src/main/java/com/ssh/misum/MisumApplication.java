3a856627d6397582130e82c6e3acaeee5d5d8507
juxnc <jcrv.909@gmail.com>
2023-12-16 00:47:28 -0600

    Basic configuration and connection to the database in PostgreSQL.

---
+++ src/main/java/com/ssh/misum/MisumApplication.java
@@ -0,0 +1,13 @@
+package com.ssh.misum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssh/misum/MisumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssh.misum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
